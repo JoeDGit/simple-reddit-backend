6378735847f4b24ed6952f4beadd06b3446a5099
JoeDGit <joe.dobbins89@gmail.com>
2022-12-12 17:38:36 +0000

    Adds completed get articleById endpoint

--- controllers/controllers.errors.js
+++ controllers/controllers.errors.js
@@ -1,8 +1,22 @@
+exports.handleBadRequests = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "400 Error - Bad Request" });
+  } else {
+    next(err);
+  }
+};
 exports.handleBadPaths = (req, res, next) => {
   res.status(404).send({ msg: "path not found" });
   next();
 };
 
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.msg && err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
 exports.handleServerErrors = (err, req, res, next) => {
   console.log(err);
   res.status(500).send({ msg: "500 server error" });
--- models/models.articles.js
+++ models/models.articles.js
@@ -6,8 +6,25 @@ exports.selectArticles = () => {
 FROM articles
 LEFT JOIN comments ON comments.article_id = articles.article_id
 GROUP BY articles.article_id
-ORDER BY created_at desc`;
+ORDER BY created_at desc;`;
   return db.query(SQL).then((articles) => {
     return articles.rows;
+  });
+};
+
+exports.selectArticleById = (articleId) => {
+  const SQL = `
+SELECT * FROM articles
+WHERE article_id = $1;`;
+
+  return db.query(SQL, [articleId]).then((article) => {
+    const selectedArticle = article.rows[0];
+    if (!selectedArticle) {
+      return Promise.reject({
+        status: 404,
+        msg: `404 Error - An article with ID ${articleId} does not exist`,
+      });
+    }
+    return selectedArticle;
   });
 };
