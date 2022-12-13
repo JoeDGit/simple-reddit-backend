792f18848b1c50e5bff908ef0f18382c4842871f
JoeDGit <joe.dobbins89@gmail.com>
2022-12-13 15:27:48 +0000

    Updates error handlers to use psql error 23503 to manage 404 errors

--- controllers/controllers.errors.js
+++ controllers/controllers.errors.js
@@ -1,6 +1,8 @@
 exports.handleBadRequests = (err, req, res, next) => {
-  if (err.code === "22P02" || err.code === "23502" || err.code === "23503") {
+  if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "400 Error - Bad Request" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "path not found" });
   } else {
     next(err);
   }
