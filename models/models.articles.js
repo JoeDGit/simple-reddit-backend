99f6d5b97041b9cec27ba2c176a9bf4d61ccfb4a
JoeDGit <joe.dobbins89@gmail.com>
2022-12-13 11:35:04 +0000

    Adds completed comments for article Id endpoint

--- models/models.articles.js
+++ models/models.articles.js
@@ -28,3 +28,25 @@ WHERE article_id = $1;`;
     return selectedArticle;
   });
 };
+
+exports.selectArticleComments = (articleId) => {
+  const SQL = `
+  SELECT * FROM comments
+  WHERE article_id = $1`;
+  return db.query(SQL, [articleId]).then((comments) => {
+    return comments.rows;
+  });
+};
+
+exports.checkIfArticleExists = (articleId) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+    .then(({ rowCount }) => {
+      if (rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `404 Error - An article with ID ${articleId} does not exist`,
+        });
+      }
+    });
+};
