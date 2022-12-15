cdd47d03cb8ca780466bb7f1608f3983df3d66de
JoeDGit <joe.dobbins89@gmail.com>
2022-12-15 10:56:22 +0000

    Adds productions changes

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,12 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -9,4 +16,4 @@ if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
