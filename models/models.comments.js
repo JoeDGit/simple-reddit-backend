64f4370352fb6d89d77c18400381f796b0a4948e
JoeDGit <joe.dobbins89@gmail.com>
2022-12-15 16:05:19 +0000

    Updates comments model to remove unused function

--- models/models.comments.js
+++ models/models.comments.js
@@ -24,16 +24,3 @@ exports.deleteCommentById = (commentId) => {
       }
     });
 };
-
-exports.checkIfCommentExists = (commentId) => {
-  return db
-    .query(`SELECT * FROM comments WHERE comment_id = $1`, [commentId])
-    .then(({ rowCount }) => {
-      if (rowCount === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: `404 Error - A comment with ID: ${commentId} does not exist`,
-        });
-      }
-    });
-};
