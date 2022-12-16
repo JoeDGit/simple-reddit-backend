bb3843d840fafd2a095b1bf5c8f8bacf4d557205
JoeDGit <joe.dobbins89@gmail.com>
2022-12-16 15:20:59 +0000

    Adds completed comment votes endpoint

--- controllers/controllers.comments.js
+++ controllers/controllers.comments.js
@@ -1,10 +1,27 @@
-const { deleteCommentById } = require('../models/models.comments');
+const {
+  deleteCommentById,
+  updateCommentVotes,
+  checkIfCommentExists,
+} = require('../models/models.comments');
 
 exports.removeCommentById = (req, res, next) => {
   const comment_id = req.params.comment_id;
   deleteCommentById(comment_id)
     .then(() => {
       res.sendStatus(204);
+    })
+    .catch(next);
+};
+
+exports.patchCommentsVotes = (req, res, next) => {
+  const voteChange = req.body.inc_votes;
+  const commentId = req.params.comment_id;
+  Promise.all([
+    updateCommentVotes(commentId, voteChange),
+    checkIfCommentExists(commentId),
+  ])
+    .then(([comment]) => {
+      res.status(200).send({ comment });
     })
     .catch(next);
 };
